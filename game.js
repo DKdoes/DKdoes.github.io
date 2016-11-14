55564d8616adef5567e01b33e3bce223844ef026
DKdoes <david.kennedy.does@gmail.com>
2016-11-13 16:57:13 -0800

    Update game.js

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ camera.position.z = 20
 window.addEventListener('resize',function(){
   renderer.setSize(window.innerWidth, window.innerHeight)
   camera.aspect = window.innerWidth / window.innerHeight
-}
+})
 window.dispatchEvent(new Event('resize'))
 
 
